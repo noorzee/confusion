49b9d9fe53dff5117ecb0468143a0ae8f43c8dad
noorzee <nhameedrocz@gmail.com>
2020-09-08 22:10:35 +0530

    Updates

---
+++ precache-manifest.a928b5c2f0e55671d514c3cfb8140cd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed2b8b8559c4d035cb277a570380554",
+    "url": "/confusion/index.html"
+  },
+  {
+    "revision": "df00594e1475d683ec75",
+    "url": "/confusion/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "8c682447a0b9dd37c4cf",
+    "url": "/confusion/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "df00594e1475d683ec75",
+    "url": "/confusion/static/js/2.551b38dd.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/confusion/static/js/2.551b38dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c682447a0b9dd37c4cf",
+    "url": "/confusion/static/js/main.a674f7e4.chunk.js"
+  },
+  {
+    "revision": "47b295cfc61f796a9c36",
+    "url": "/confusion/static/js/runtime-main.19a3f2b7.js"
+  }
+]);
